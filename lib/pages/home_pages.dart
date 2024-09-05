8d1f218854a2ca39688246e8fa8b3099acd4bffd
Gmoisesqa26 <gmoisesqa26@gmail.com>
2024-09-05 11:07:23 -0600

    AppBar added

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+|class HomePages extends StatefulWidget {
+  const HomePages({super.key});
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 3, 2, 102),
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+}
