6898f5803f3fd6064ed8daf573a9b717eb7a9bee
Gmoisesqa26 <gmoisesqa26@gmail.com>
2024-09-09 13:23:15 -0600

    MyTab insertado

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_geisler_quintal/Utilis/my_tab.dart';
 
-|class HomePages extends StatefulWidget {
+class HomePages extends StatefulWidget {
   const HomePages({super.key});
 
   @override
@@ -8,15 +9,71 @@ import 'package:flutter/material.dart';
 }
 
 class _HomePagesState extends State<HomePages> {
+  List<Widget> myTabs = [
+    //donutTab
+    const MyTab(
+      iconPath: 'lib/icons/donut.png',
+    ),
+    //burgerTab
+    const MyTab(
+      iconPath: 'lib/icons/burger.png',
+    ),
+    //smoothieab
+    const MyTab(
+      iconPath: 'lib/icons/smoothie.png',
+    ),
+    //pancakeTab
+    const MyTab(
+      iconPath: 'lib/icons/pancakes.png',
+    ),
+    //pizzaTab
+    const MyTab(
+      iconPath: 'lib/icons/pizza.png',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 3, 2, 102),
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey,
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+            ),
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
         ),
+        body: Column(children: [
+          //texto " I want to it"
+          const Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text("I want to ", style: TextStyle(fontSize: 24)),
+                Text("Eat",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline)),
+              ],
+            ),
+          ),
+          //Tab bar
+          TabBar(tabs: myTabs)
+          //Tab bar view
+
+          //Total del carrito
+        ]),
       ),
     );
   }
